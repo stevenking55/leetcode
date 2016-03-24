41f2a663cc4513127ebac527017b7bfc01991238
stevenking55 <sjin.stevenking55@gmail.com>
2016-03-24 15:15:04 +0800

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,1 +1,21 @@
-1.2.3.c.
+#include <stdio.h>
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target) {
+    int i = 0;
+    int j = 0;
+    int *ret = (int *)malloc(sizeof(int)*2);
+    for (i=0; i<numsSize; i++){
+        for (j=i+1; j<numsSize; j++){
+            if (target == *(nums+i) + *(nums+j)){
+                if(i != j){
+                    ret[0] = i;
+                    ret[1] = j;
+                    return ret;
+                }
+            }
+        }
+    }
+    return NULL;
+}
+
